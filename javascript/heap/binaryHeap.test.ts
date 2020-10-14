df3d9d2f1e11d0a338af8fc7c7a59b4384a1bb78
Patrick Jones <patrickjones.pmj@gmail.com>
2020-10-14 00:29:41 -0500

    clean up

--- javascript/heap/binaryHeap.test.ts
+++ javascript/heap/binaryHeap.test.ts
@@ -1,5 +1,5 @@
 import BinaryHeap, { MaxHeap, MinHeap } from "./binaryHeap"
-import { before, describe, it } from "mocha";
+import { beforeEach, describe, it } from "mocha";
 import { assert, expect } from "chai";
 describe('BinaryHeap', () => {
     var heap;
@@ -9,27 +9,43 @@ describe('BinaryHeap', () => {
 
         done()
     })
-    describe('Default', function() {
-        it('create a BinaryHeap', function() {
-            assert.instanceOf(heap, BinaryHeap)
-        })
-        it('return a MaxHeap by default', function() {
-            expect(compareDirectionIsMax(heap.compare)).to.be.true
-        })
+    it('create a BinaryHeap', function() {
+        assert.instanceOf(heap, BinaryHeap)
     })
+    it('return a MaxHeap by default', function() {
+        expect(compareDirectionIsMax(heap.compare)).to.be.true
+    })
+    
     describe('MaxHeap Class', () => {
         it('create a MaxHeap', function() {
             assert.instanceOf(heap, BinaryHeap)
             expect(compareDirectionIsMax(heap.compare)).to.be.true
         })
     })
-
     describe('MinHeap', () => {
         let minHeap = new MinHeap([]);
 
         it('create a MinHeap', function() {
             assert.instanceOf(heap, BinaryHeap)
             expect(compareDirectionIsMin(minHeap.compare)).to.be.true
+        })
+    })
+    describe('#peek()', () => {
+        let peekHeap = new MaxHeap([5])
+        it('returns the top element in the heap', function() {
+            expect(peekHeap.peek).to.equal(peekHeap.data[0])
+        })
+        it('returns null when the heap is empty', function() {
+            expect(peekHeap.pop()).to.equal(5)
+            expect(peekHeap.peek).to.be.null
+
+        })
+    })
+    describe('#push()', () => {
+        let pushHeap = new MaxHeap([])
+        it('adds elements the heap', function() {
+            pushHeap.push(5)
+            expect(pushHeap.peek).to.equal(5)
         })
     })
     
